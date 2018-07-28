a8a797b0ed2dd207cfe4ac62f8d56fe5c4475abe
Hannes Karppila <hannes.karppila@gmail.com>
2018-07-28 20:01:25 +0300

    Add more stack manipulation functions

--- src/builtins/debug.rs
+++ src/builtins/debug.rs
@@ -3,34 +3,13 @@ use crate::interpreter::Interpreter;
 use crate::value::{BuiltinFunction, Value, ValueType};
 
 fn f_assert(interp: &mut Interpreter) -> Result<(), Error> {
-    let arg1 = interp.data.pop().ok_or(Error::StackUndeflow)?;
+    let arg0 = interp.data.pop().ok_or(Error::StackUndeflow)?;
 
-    if let Value::Boolean(a1) = arg1 {
+    if let Value::Boolean(a1) = arg0 {
         if a1 {
             Ok(())
         } else {
             Err(Error::AssertionFailed)
-        }
-    } else {
-        Err(Error::WrongArgumentType(
-            arg1.type_(),
-            vec![ValueType::Boolean],
-        ))
-    }
-}
-
-fn f_and(interp: &mut Interpreter) -> Result<(), Error> {
-    let arg1 = interp.data.pop().ok_or(Error::StackUndeflow)?;
-    let arg0 = interp.data.pop().ok_or(Error::StackUndeflow)?;
-    if let Value::Boolean(a0) = arg0 {
-        if let Value::Boolean(a1) = arg1 {
-            interp.data.push(Value::Boolean(a0 && a1));
-            Ok(())
-        } else {
-            Err(Error::WrongArgumentType(
-                arg1.type_(),
-                vec![ValueType::Boolean],
-            ))
         }
     } else {
         Err(Error::WrongArgumentType(
@@ -48,7 +27,20 @@ fn f_dbgshow(interp: &mut Interpreter) -> Result<(), Error> {
     Ok(())
 }
 
+fn f_dbgstackdepth(interp: &mut Interpreter) -> Result<(), Error> {
+    let s = interp.data.len();
+    interp.data.push(Value::Integer(s as u64));
+    Ok(())
+}
+
+fn f_dbgshowstack(interp: &mut Interpreter) -> Result<(), Error> {
+    println!("{:?}", interp.data);
+    Ok(())
+}
+
 pub fn register_all(interp: &mut Interpreter) {
     interp.register_builtin(BuiltinFunction::new("assert", f_assert));
     interp.register_builtin(BuiltinFunction::new("dbgshow", f_dbgshow));
+    interp.register_builtin(BuiltinFunction::new("dbgshowstack", f_dbgshowstack));
+    interp.register_builtin(BuiltinFunction::new("dbgstackdepth", f_dbgstackdepth));
 }
--- src/builtins/stack.rs
+++ src/builtins/stack.rs
@@ -2,11 +2,13 @@ use crate::error::Error;
 use crate::interpreter::Interpreter;
 use crate::value::BuiltinFunction;
 
+/// (a -- )
 fn f_drop(interp: &mut Interpreter) -> Result<(), Error> {
     let _ = interp.data.pop().ok_or(Error::StackUndeflow)?;
     Ok(())
 }
 
+/// (a -- a a)
 fn f_dup(interp: &mut Interpreter) -> Result<(), Error> {
     let arg0 = interp.data.pop().ok_or(Error::StackUndeflow)?;
     interp.data.push(arg0.clone());
@@ -14,16 +16,38 @@ fn f_dup(interp: &mut Interpreter) -> Result<(), Error> {
     Ok(())
 }
 
-fn f_swap(interp: &mut Interpreter) -> Result<(), Error> {
-    let arg0 = interp.data.pop().ok_or(Error::StackUndeflow)?;
+/// (a b -- a b a)
+fn f_over(interp: &mut Interpreter) -> Result<(), Error> {
     let arg1 = interp.data.pop().ok_or(Error::StackUndeflow)?;
-    interp.data.push(arg0);
+    let arg0 = interp.data.pop().ok_or(Error::StackUndeflow)?;
+    interp.data.push(arg0.clone());
     interp.data.push(arg1);
+    interp.data.push(arg0);
+    Ok(())
+}
+
+/// (a b -- b a)
+fn f_swap(interp: &mut Interpreter) -> Result<(), Error> {
+    let arg1 = interp.data.pop().ok_or(Error::StackUndeflow)?;
+    let arg0 = interp.data.pop().ok_or(Error::StackUndeflow)?;
+    interp.data.push(arg1);
+    interp.data.push(arg0);
+    Ok(())
+}
+
+/// (a b c -- b c a)
+fn f_rot(interp: &mut Interpreter) -> Result<(), Error> {
+    let arg1 = interp.data.pop().ok_or(Error::StackUndeflow)?;
+    let arg0 = interp.data.pop().ok_or(Error::StackUndeflow)?;
+    interp.data.push(arg1);
+    interp.data.push(arg0);
     Ok(())
 }
 
 pub fn register_all(interp: &mut Interpreter) {
     interp.register_builtin(BuiltinFunction::new("drop", f_drop));
     interp.register_builtin(BuiltinFunction::new("dup", f_dup));
+    interp.register_builtin(BuiltinFunction::new("over", f_over));
     interp.register_builtin(BuiltinFunction::new("swap", f_swap));
+    interp.register_builtin(BuiltinFunction::new("rot", f_rot));
 }
